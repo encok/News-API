2382ee0c438e6aa46ecca0cd4b8df31c7340069e
enock <enckkipronoh@gmail.com>
2021-09-10 17:08:34 +0300

    adding the control flows

--- app/views.py
+++ app/views.py
@@ -8,13 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    bbc = 'Hello World'
-    return render_template('index.html',bbc = bbc)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
 
 @app.route('/source/<int:source_id>')
-def source(source_id):
+def source():
 
     '''
     View source page function that returns the source details page and its data
     '''
-    return render_template('source.html',id = source_id)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('source.html', title = title)
