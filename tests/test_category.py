ea8d1e909fac8eff554972c7d0f44efda5140941
enock <enckkipronoh@gmail.com>
2021-09-11 14:26:51 +0300

    testing for category done

--- tests/test_category.py
+++ tests/test_category.py
@@ -18,4 +18,5 @@ class CategoryTest(unittest.TestCase):
         '''
         self.assertTrue(isinstance(self.new_category,Category))
 
-# isinstance:::: Return true if the object argument is an instance of the classinfo argument, or of a (direct, indirect or virtual) subclass thereof. If object is not an object of the given type, the function always returns false.
+
+        
